93896e121934585348fc9e8dab012706c218a326
giacomo1989 <voltolina.giacomo89@gmail.com>
2018-01-26 13:56:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def processRequest(req):
 		celsius=int(condition.get('temp'))
 		Gc=int((celsius-32)/1.8)
        		
-		speech1 = city
+		speech1 =day+", "+date+"\n\nToday in " + location.get('city') + ": " + condition.get('text') + ".\nThe temperature is " + str(Gc)+ " C with a humidity of "+umidita+"%"
 		res = makeWebhookResult(speech1)
 		return res
 		
